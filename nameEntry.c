72e24af864880e06c3f37ad80380bb4fd222ac39
Joe Dickerson <joetango97@gmail.com>
2015-12-11 12:51:13 -0500

    Improvments
    Now the program doesn't close when the user enters too many characters.

--- nameEntry.c
+++ nameEntry.c
@@ -12,19 +12,30 @@ int main ()
     FILE *f; //File pointer declaration
 
     printf("What is your name?\n");
-    printf("(please enter 16 characters or less and no spaces)\nNAME:");
-    scanf("%s" ,userName);
-    
-    for(i=0; userName[i]!='\0'; ++i); //Finds the length of the string userName and sets it to i
+    printf("NAME:");
+  
 
-    if(i>16){
-    printf("Please restart the program and only use 16 characters or less\n");
+
+    //OLD METHOD, leaving it here for now
+    // scanf("%s" ,userName); 
+    // for(i=0; userName[i]!='\0'; ++i); //Finds the length of the string userName and sets it to i
+
+
+
+    //The point of the loop and if statement is so if the user inputs too many characters, rather than close the program 
+    //the user will just get another scanf
+    for(i=i;i>16;i){
+	scanf("%s" ,userName); //User input
+	for(i=0; userName[i]!='\0'; ++i); //Makes length of string integer and sets it to i
+	if (i>16){ 
+	    printf("Please use 16 characters or less.\n");
+	}
     }
-    else{
+   
 	printf("You entered: %s\n" , userName);
 
 	f = fopen("save.txt" , "w"); //Opens or creates file 
 	fprintf(f, "%s\n" , userName); //Writes value of userName to file 
 	fclose(f); //Closes file
-    }
+    
 }//main 
